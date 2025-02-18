7809f6efac445ab5d3d9a7651902a97336ac501a
dudwns1927 <dudwns1927@gmail.com>
2025-02-18 19:44:38 +0900

    (2025.02.18) 스택

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -30,50 +30,114 @@ public:
 
     ~DoubleLinkedList()
     {
-
+        Node* current = head, tail;
+        
+        while (current != nullptr)
+        {
+            Node* temp = current;
+            current = current->next;
+            delete temp;
+        }
     }
 
-    int getsize() const
-    {
-        return size;
-    }
+    /*
+    ~DoubleLinkedList()
+    {    
+        while (head != nullptr)
+        {
+            pop_front()
+        }
+    }*/
 
     void push_front(T data)
     {
         Node* newNode = new Node;
 
+        newNode->data = data;
+        newNode->next = nullptr;
+        newNode->previous = nullptr;
+
         if (head == nullptr)
         {
             head = newNode;
-
-            newNode->data = data;
-            newNode->next = nullptr; //시작주소를 가리킨다.
-        }
-        else if (tail == nullptr)
-        {
             tail = newNode;
-
-            newNode->data = data;
-            newNode->next = nullptr;
         }
-        else
+        else 
         {
-            Node* currentNode = head;
-            Node* currentNode = tail;
+            head->previous = newNode;
+            newNode->next = head;
 
-            while (currentNode->next != nullptr)
-            {
-                currentNode = currentNode->next;
-            }
-
-            currentNode->next = newNode;
-
-            newNode->data = data;
-            newNode->next = nullptr;
+            head = newNode;
         }
 
         size++;
     }
+
+    void push_back(T data)
+    {
+        Node* newNode = new Node;
+
+        newNode->data = data;
+        newNode->next = nullptr;
+        newNode->previous = nullptr;
+
+        if (tail == nullptr)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+        else
+        {
+            tail->next - newNode;
+            newNode->previous = tail;
+            
+            tail = newNode;
+        }
+
+        size++;
+    }
+
+    void show()
+    {
+        Node* currentNode = head;
+
+        while (currentNode != nullptr)
+        {
+            cout << currentNode->data << " ";
+
+            currentNode = currentNode->next;
+        }
+    }
+
+
+    void pop_front()
+    {
+        if (head == nullptr)
+        {
+            cout << "Linked List is Empty" << endl;
+        }
+        else
+        {
+            Node* deleteNode = head;
+
+            if (head == tail)
+            {
+                head = nullptr;
+                tail = nullptr;
+            }
+            else
+            {
+                deleteNode->next->previous = nullptr;
+
+                head = head->next;
+            }
+
+            delete deleteNode;
+
+            size--;
+        }
+    }
+
 };
 
 
@@ -85,8 +149,21 @@ int main()
 {
     DoubleLinkedList<int> doubleLinkedList;
 
-    doubleLinkedList.push_front(10);
+    doubleLinkedList.push_front(30);
     doubleLinkedList.push_front(20);
+    doubleLinkedList.push_front(10);
+
+    doubleLinkedList.pop_front();
+    doubleLinkedList.pop_front();
+    doubleLinkedList.pop_front();
+    doubleLinkedList.pop_front();
+
+    doubleLinkedList.push_back(10);
+    doubleLinkedList.push_back(20);
+    doubleLinkedList.push_back(30);
+    
+
+    doubleLinkedList.show();
 
 
     return 0;
