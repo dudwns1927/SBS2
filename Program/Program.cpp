81327a103249721c00d1c89b5a8ecae00b96a4f5
jaeyoon <dudwns1927@gmail.com>
2025-02-27 19:45:38 +0900

    (2025.2.27) 이진 트리

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -1,124 +1,132 @@
 ﻿
 #include <iostream>
-
-//#define SIZE 4
+#include <cstring>
 
 using namespace std;
-template <typename T>
 
 
-class Vector
+
+class String
 {
 private:
     int size;
-    int capacity;
+    char* pointer;
+    
+public:    
 
-    T* container;
+    String()
+    {   
+        size = 0;        
+        pointer = nullptr;
+    }
 
 
-public:
-    Vector()
+    String(const char* str)
     {
         size = 0;
-        capacity = 0;
-        container = nullptr;
+        pointer = nullptr;
+        if (str != nullptr)
+        {
+            size = strlen(str);
+            pointer = new char[size + 1];
+            strcpy_s(pointer, size+1, str);
+        }
     }
 
-    ~Vector()
+    ~String()
     {
-        delete[] container;
-    }
-    
-    void resize(int newSize)
-    {
-        // 1. capacity에 새로운 size 값을 저장합니다.
-        capacity = newSize;
-
-        // 2. 새로운 포이널 변수를 생성해서 새롭게 만들어진
-        //    메모리 공간을 가리키도록 합니다.
-        T* newContainer = new T[capacity];
-
-        // 3. 새로운 메모리 공간의 값을 초기화 합니다.
-        for (int i = 0; i < capacity; i++)
+        if (pointer != nullptr)
         {
-            newContainer[i] = NULL;
-        }
-
-        // 4. 기존 배열에 있는 값을 복사해서 새로운 배열에 넣어줍니다.
-        for (int i = 0; i < size; i++)
-        {
-            newContainer[i] = container[i];
-        }
-
-        // 5. 기존 배열의 메모리 해제합니다.
-        if (container != nullptr)
-        {
-            delete[] container;
+            delete[] pointer;
         }
         
-        // 6. 기존에 배열을 가리키던 포인터 변수의 값을
-        //    새로운 배열의 시작 주소로 가리킵니다.
-        container = newContainer;
-
     }
 
-    void push_back(T data)
-    {
-        if (capacity <= 0)
-        {
-            resize(1);
-        }
-        else if (size >= capacity)
-        {
-            resize(capacity * 2);
-        }
-        container[size++] = data;
-    }
-
-    void pop_back()
-    {
-        if (size <= 0)
-        {
-            cout << "vector is empty" << endl;
-        }
-        else
-        {
-            container[--size] = NULL;
-           
-        }
-    }
-
-    
-
-
-    const int& Size()
+    int length() const
     {
         return size;
     }
 
-    const T& operator [] (const int& index)
+    void operator = (const char* word)
     {
-        return container[index];
+        size = strlen(word) + 1;
+        if (pointer == nullptr)
+        {
+            pointer = new char[size];
+                
+            for (int i = 0; i < size; i++)
+            {
+                pointer[i] = word[i];
+            }
+        }
+        else
+        {
+            char* newPointer = new char[size];
+            
+            for (int i = 0; i < size; i++)
+            {
+                newPointer[i] = word[i];
+            }
+
+            delete[] pointer;
+
+            pointer = newPointer;
+        }
     }
+
+    int Size() const
+    {     
+        return size - 1;
+    }
+
+    const char& operator [] (const int& index)
+    {
+        return pointer[index];
+    }
+
+    char& operator[](int index)
+    {
+        if (index >= 0 && index < size - 1)  // NULL 문자를 제외한 인덱스를 처리
+        {
+            return pointer[index];
+        }
+        throw out_of_range("Index out of range");
+    }
+
+   void print() const
+   {
+       if (pointer != nullptr)
+       {
+           cout << pointer << endl;
+       }
+       else
+       {
+           cout << "Empty String" << endl;
+       }
+   }
 };
 
 int main()
 {
-    Vector<int> vector;
+    
+    String string;
 
-    vector.push_back(10);
-    vector.push_back(20);
-    vector.push_back(30);
-    vector.push_back(40);
-    vector.push_back(50);
+    string = "Key";
 
-    for (int i = 0; i < vector.Size(); i++)
+    for (int i = 0; i < string.Size(); i++)
     {
-        cout << vector[i] << " ";
-
+        cout << string[i];        
     }
 
+    cout << endl;
+    
+    string = "apple";
 
+    for (int i = 0; i < string.Size(); i++)
+    {
+        cout << string[i];        
+    }
+    
 
     return 0;
 }
