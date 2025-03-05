74e8c2bcb99850071b81a3bd50aafba00d8651d6
dudwns1927 <dudwns1927@gmail.com>
2025-03-05 19:45:25 +0900

    (2025.3.5) 균형 이진 트리

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -1,129 +1,57 @@
 ﻿#include <iostream>
+#include <queue>
 
-#define SIZE 6
+#define SIZE 8
 
 using namespace std;
 
-template<typename KEY, typename VALUE>
-class HashTable
+template<typename T>
+
+
+class PriorityQueue
 {
 private:
-    struct Node
-    {
-        KEY key;
-        VALUE value;
+    int index;
+    T container[SIZE];
 
-        Node* next;
-    };
-
-    struct Bucket
-    {
-        int count;
-        Node* head;
-    };
-
-    Bucket bucket[SIZE];
 public:
-    HashTable()
+    PriorityQueue()
     {
+        index = 0;
+
         for (int i = 0; i < SIZE; i++)
         {
-            bucket[i].count = 0;
-            bucket[i].head = nullptr;
+            container[i] = NULL;
         }
     }
 
-    ~HashTable()
+    ~PriorityQueue()
     {
-        for (int i = 0; i < SIZE; i++)
-        {
-            Node* deleteNode = bucket[i].head;
-            Node* nextNode = bucket[i].head;
 
-            if (bucket[i].head == nullptr)
-            {
-                continue;
-            }
-            else
-            {
-                while (nextNode != nullptr)
-                {
-                    nextNode = deleteNode->next;
-
-                    delete deleteNode;
-
-                    deleteNode = nextNode;
-                }
-            }
-        }
-    }
-
-    template <typename T>
-    const int& HashFunction(T key)
-    {
-        unsigned int hashIndex = (int)key % SIZE;
-
-        return hashIndex;
-    }
-
-    template<>
-    const int& HashFunction(const char* key)
-    {
-        int hash = 0;
-
-        for (int i = 0; i < strlen(key); i++)
-        {
-            hash += key[i];
-        }
-
-        int hashIndex = hash % SIZE;
-
-        return hashIndex;
-    }
-
-    Node* CreateNode(KEY key, VALUE value)
-    {
-        Node* newNode = new Node;
-
-        newNode->key = key;
-        newNode->value = value;
-        newNode->next = nullptr;
-
-        return newNode;
-    }
-
-    void Insert(KEY key, VALUE value)
-    {
-        // 해시 함수를 통해서 값을 받는 임시 변수
-        int hashIndex = HashFunction(key);
-
-        // 새로운 노드를 생성합니다.
-        Node* newNode = CreateNode(key, value);
-
-        // 노드가 1개라도 존재하지 않는다면
-        if (bucket[hashIndex].count == 0)
-        {
-            // bucket[hashIndex]의 head 포인터가 newNode를 가리키게 합니다. 
-            bucket[hashIndex].head = newNode;
-        }
-        else
-        {
-            newNode->next = bucket[hashIndex].head;
-
-            bucket[hashIndex].head = newNode;
-        }
-
-        // bucket[hashIndex]의 count를 증가시킵니다.
-        bucket[hashIndex].count++;
     }
 };
 
 int main()
 {
-    HashTable<const char*, int> hashTable;
+    priority_queue<int> priorityQueue;
 
-    hashTable.Insert("Sword", 10000);
-    hashTable.Insert("Armor", 5000);
+    //priorityQueue.push(30);
+    //priorityQueue.push(2);
+    //priorityQueue.push(77);
+    //priorityQueue.push(15);
+    //
+    //int size = priorityQueue.size();  
+    //for (int i = 0; i < size; ++i) {
+    //    std::cout << priorityQueue.top() << std::endl;
+    //    priorityQueue.pop(); 
+    //}
+    //
+    //while (priorityQueue.empty() == false)
+    //{
+    //    cout << priorityQueue.top() << endl;
+    //
+    //    priorityQueue.pop();
+    //}
 
     return 0;
 }
